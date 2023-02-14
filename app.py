b386e0fa0237487f2734e942497c2c19bff17e52
GTdllab <35949093+GTdllab@users.noreply.github.com>
2023-02-14 07:38:53 +0000

    upload csv

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-#-----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 
-from flask import Flask
+
+from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        df = pd.read_csv(request.files.get('file'))
+        return render_template("index.html", shape=df.shape, tables=[df.head().to_html()], titles=[''])
+    return render_template("index.html")
